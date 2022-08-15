aaca0fb458f3a5b8dc012b6b3c8bb3ce8e033808
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-15 20:39:37 +0530

    Added:The program of Bubble Sort using Generic is added

--- src/main/java/com/bridgelabz/GenericBubbleSort.java
+++ src/main/java/com/bridgelabz/GenericBubbleSort.java
@@ -1,29 +1,31 @@
 /*
-Bubble Sort
+Bubble Sort using Generic
  */
 
 package com.bridgelabz;
 
-public class BubbleSort {
+public class GenericBubbleSort {
     public static void main(String[] args) {
-        BubbleSort sort = new BubbleSort();
-        int array[] = {5,93,58,74,87,93,10,53,97};
+        GenericBubbleSort sort = new GenericBubbleSort();
+        String sentence = "can,I,help,you";
+        String array[] = sentence.toLowerCase().split(",");
         sort.bubbleSort(array);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
     }
 
-    public void bubbleSort(int[] array) {
-        int temp;
+    public <K extends Comparable<K>> K bubbleSort(K[] array) {
+        K temp = null;
         for (int i = 0; i < array.length; i++) {//Outer for loop
             for (int j = 0; j < array.length - i - 1; j++) {//Inner for loop
-                if (array[j] > (array[j + 1])) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
                     temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
             }
         }
+        return temp;
     }
 }
